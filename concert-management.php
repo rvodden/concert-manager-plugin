07edf57744c7f8f1decc54c620c9f54bac219272
Richard Vodden <richard@vodden.com>
2016-12-22 09:57:49 +0000

    Setup unit testing framework

---
+++ concert-management.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ Plugin Name: Concert Manager
+ Plugin URI: http://www.vodden.com/concertmanager
+ Description: This is to make maintaining musical ensembles websites much more easily!
+ Version: 0.0.1
+ Author: Richard Vodden
+ Author URI: http://www.vodden.com/
+ License: GPL
+ */
+?>
