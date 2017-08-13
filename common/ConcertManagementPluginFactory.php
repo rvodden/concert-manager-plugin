7938ec1e72d092b5d1b48ecee4b4105f75de0d4e
Richard Vodden <richard@vodden.com>
2017-08-13 22:56:41 +0100

    missed out a file!

---
+++ common/ConcertManagementPluginFactory.php
@@ -0,0 +1,33 @@
+<?php
+namespace uk\org\brentso\concertmanagement\common;
+
+use uk\org\brentso\concertmanagement\ConcertManagementPublic;
+use uk\org\brentso\concertmanagement\admin\Admin;
+
+/**
+ *
+ * @author voddenr
+ *
+ */
+class ConcertManagementPluginFactory {
+
+	public static function createPlugin() {
+		
+		$plugin_name = 'concert-management';
+		$plugin_version = '0.0.1';
+		
+		$loader = new Loader();
+		$concertManagementPublic = new ConcertManagementPublic( $plugin_name, $plugin_version );
+		$admin = new Admin( $plugin_name, $plugin_version );
+		$i18n = new I18n();
+		
+		$concertManagementPlugin = new ConcertManagementPlugin(
+			$loader,
+			$concertManagementPublic,
+			$admin,
+			$i18n
+		);
+		
+		return $concertManagementPlugin;
+	}
+}
